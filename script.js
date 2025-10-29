0aaab3ca4d23921e8569280305c1f1421c13c3f2
Otavio-Sales-N29 <otavio.sales.pereira@escola.pr.gov.br>
2025-10-29 20:32:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function likeButtonRender() {
 
 function initializeSong() {
     cover.src = `images/${sortedPlaylist[index].file}.jpg`;
-    song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+    song.src = `mysongs/${sortedPlaylist[index].file}.mp3`;
     songName.innerText = sortedPlaylist[index].songName;
     bandName.innerText = sortedPlaylist[index].artist;
     likeButtonRender();
